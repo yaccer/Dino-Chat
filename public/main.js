f8f8bea4c5fd580502bbf68c7fe4820583dd60f7
yaccer <yaseen.ahmad@ada.ac.uk>
2019-11-29 04:17:55 +0000

    create simple dino menu

--- public/main.js
+++ public/main.js
@@ -1,14 +1,18 @@
 const randomInt = Math.floor(Math.random() * 2);
 document.cookie = `randomInt=${randomInt}`;
 
+const body = document.querySelector("body");
+const dino = document.getElementById("dino");
+
 if (randomInt === 0) {
-    document.getElementById("dino").src = "/assets/trex.gif";
-    document.querySelector("body").style.background = "#cbcbcb";
+    body.style.background = "#cbcbcb";
+    dino.src = "/assets/trex.gif";
 } else {
-    document.getElementById("dino").src = "/assets/bronto.gif";
-    document.querySelector("body").style.background = "#ffffff";
+    body.style.background = "#ffffff";
+    dino.src = "/assets/bronto.gif";
+    dino.style.border = "solid 2px #000000";
 }
 
 document.addEventListener("click", async () => {
-    window.location.replace("/chat");
+    window.location.href = "/chat";
 });
